564d0225ea34d37aee0f23e7ba217ab623d59941
jeonghoon11 <jih361@naver.com>
2025-11-18 01:59:53 +0900

    fix: 경로 상수화

--- Week4/homework/src/pages/home/home.tsx
+++ Week4/homework/src/pages/home/home.tsx
@@ -1,10 +1,11 @@
+import { ROUTE_PATH } from "@shared/router/path";
 import * as styles from "./home.css.ts";
 import { Link } from "react-router";
 
 const Home = () => {
   return (
     <div className={styles.container}>
-      <Link to="/login" className={styles.link}>
+      <Link to={ROUTE_PATH.LOGIN} className={styles.link}>
         로그인 하러가기
       </Link>
     </div>
